e5af6c909ef0c7d889149ff5a9880cc714e911f2
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2023-03-27 01:25:30 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ultimately Boring MIDI Editor</h1>
+    </div>
+  )
+}
+
+export default App
